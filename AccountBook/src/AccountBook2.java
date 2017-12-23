dbc6caba111c2421b4b6749ec430aa6c3a527b64
SohhyangLee <sohhyang7@gmail.com>
2017-12-23 17:25:54 +0900

    delete 수정

--- AccountBook/src/AccountBook2.java
+++ AccountBook/src/AccountBook2.java
@@ -1,9 +1,17 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import sun.rmi.runtime.Log;
 
 public class AccountBook2 {
 
@@ -51,7 +59,7 @@ public class AccountBook2 {
 		Scanner scan = new Scanner(System.in);
 		try {
 			FileWriter output = new FileWriter("accountbook.txt", true);
-			System.out.print("추가할 내용을 입력해주세요(숫자/이름/가격 순으로) : ");
+			System.out.print("추가할 내용을 입력해주세요(이름 날짜 가격 순으로) : ");
 			String accountbook = scan.nextLine();
 			String data = accountbook + "\r\n";
 			output.write(data);
@@ -63,19 +71,90 @@ public class AccountBook2 {
 		System.out.println();
 	}
 	void UpdateAccountBook(){
-		System.out.print("test2");
-		System.out.println();
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("update할 것을 입력하세요: ");
+		String a = scan.nextLine();
+		System.out.print("update할 물건을 입력하세요: ");
+		String b = scan.nextLine();
+		
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("accountbook.txt"));
+		
+			String kword = null;
+			String eword = null;
+			
+			while((eword = in.readLine())!=null) {				
+				if(eword.contains(a)) {	
+					kword = eword;
+			
+				}
+			}
+		
+		if(kword == null)
+			System.out.println(a+" 와 일치하는 가계부는 없습니다.");
+		else
+			System.out.println(kword + " 항목을 찾았습니다.");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	void DeleteAccountBook(){
-		System.out.print("test3");
-		System.out.println();
+		
+		String line;
+		String kword = null;
+
+		try {
+			
+			Scanner scan = new Scanner(System.in);
+			System.out.print("삭제할 물건을 입력하세요 : ");
+			String del = scan.nextLine();
+			
+			BufferedReader br = new BufferedReader(new FileReader("accountbook.txt"));
+			File file = new File("re_accountbook.txt");
+			
+			file.createNewFile();
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			while((line=br.readLine())!=null) {
+				if(!line.contains(del)) {
+					bw.write(line);
+					bw.write("\n");
+				}
+				else
+					kword = del;
+				bw.flush();
+			}
+			if(kword == null)
+				System.out.println(del+" 과(와) 일치하는 것은 없습니다. 다시 입력해주세요.");
+			
+			FileInputStream fis = new FileInputStream("re_accountbook.txt");
+			FileOutputStream fos = new FileOutputStream("accountbook.txt");
+			   
+			   int data = 0;
+			   while((data=fis.read())!=-1) {
+			    fos.write(data);
+			   }
+			   fis.close();
+			   fos.close();
+
+			
+			bw.close();
+			fw.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+			
 	}
 	void ShowAccountBook(){
 		try {
 			BufferedReader output = new BufferedReader(new FileReader("accountbook.txt"));
 			while(true) {
 				String accountbook = output.readLine();
-				if (accountbook == null) break;
+				if (accountbook == null) 
+					break;
 				System.out.println(accountbook);
 			}
 			output.close();
